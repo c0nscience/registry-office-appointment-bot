592b409238f28726471093088574f2b4c454827d
Benjamin Herzig <ber.bherzig@gmail.com>
2019-02-10 20:11:47 +0100

    update gitignore

--- handler.py
+++ handler.py
@@ -1,10 +1,24 @@
+from bs4 import BeautifulSoup
+import requests
 import json
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
+}
 
 
 def hello(event, context):
+    params = {
+        'termin': 1,
+        'dienstleisterlist': '122210',
+        'anliegen': '120335',
+    }
+    response = requests.get('https://service.berlin.de/terminvereinbarung/termin/tag.php', params=params,
+                            headers=headers)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
     body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
+        "title": soup.title.string
     }
 
     response = {
